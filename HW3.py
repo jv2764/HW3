e737e2814e8e4751f3af95d11061f741c7c67567
John Vu <31754493+jv2764@users.noreply.github.com>
2017-09-29 22:27:40 -0700

    Add files via upload

---
+++ HW3.py
@@ -0,0 +1,42 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+3 people : Harry, Sally, Mary
+    if Harry and Sally are both less than 20 years old
+    display message saying : "Either Sally and Harry are less than 20 years old"
+    if Sally or Mary is older than 30
+    display message saying : "Either Sally or Mary is older than 30 "
+
+
+# In[ ]:
+
+user enter first name
+compute numbers of the characters
+print
+
+
+# In[ ]:
+
+user enter favorite movie
+reverse the name of favorite movie
+print
+
+
+# In[ ]:
+
+user enter line of text
+user search string
+convert user line of text to lowercase
+search the string result
+print "Search String Found"
+if search string wasn't found
+print "Search String Not Found"
+
+
+# In[ ]:
+
+import string
+milk,bannas,sugar,eggs,chicken
+
